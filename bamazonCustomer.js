d85f75d9d430094625bfebb9af3909597854eba7
Mike Worthen <mikeworthen55@gmail.com>
2019-07-28 22:51:49 -0600

    prompts have been added.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 
+var inquirer = require("inquirer");
+
 let connection = mysql.createConnection( {
     host: "localhost",
     port: 8889,
@@ -21,5 +23,24 @@ function displayItems() {
         console.log(res[i].Item_ID + "  | " + res[i].Product_Name + " | $" + res[i].Price);
       }
       console.log("---------------------------------------------------------");
-    });
-  }
+
+      inquirer
+      .prompt([
+        {
+          name: "ID",
+          type: "input",
+          message: "What item would you like (Enter the ID number)?"
+        },
+        {
+          name: "ID",
+          type: "input",
+          message: "How many would you like?"
+        },
+        {
+          name: "ID",
+          type: "input",
+          message: "Is this quantity correct?"
+        }
+      ]);
+  });
+}
