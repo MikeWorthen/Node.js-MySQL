a5fb07118627a5fa2fc84627c047ea2a6c3e1904
Mike Worthen <mikeworthen55@gmail.com>
2019-07-29 21:14:38 -0600

    working but taking the info from number ten in

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,16 @@ var mysql = require("mysql");
 
 var inquirer = require("inquirer");
 
+var userID;
+var userQuantity;
+
 let connection = mysql.createConnection( {
     host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
     database: "bamazonDB",
+
 });
 
 connection.connect(function (err) {
@@ -20,7 +24,7 @@ function displayItems() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].Item_ID + "  | " + res[i].Product_Name + " | $" + res[i].Price);
+        console.log(res[i].Item_ID + "  | " + res[i].Product_Name + " | $" + res[i].Price + " | " + res[i].Stock_Quantity); 
       }
       console.log("---------------------------------------------------------");
 
@@ -32,15 +36,57 @@ function displayItems() {
           message: "What item would you like (Enter the ID number)?"
         },
         {
-          name: "ID",
+          name: "Quantity",
           type: "input",
           message: "How many would you like?"
         },
         {
-          name: "ID",
-          type: "input",
-          message: "Is this quantity correct?"
+          name: "confirm",
+          type: "confirm",
+          message: "Is this quantity correct?",
+          default: true,
         }
-      ]);
+      ])
+      .then(function(userInput) {
+        var itemCount;
+        var total; 
+        var itemsRemaining;
+        var itemsOrdered = parseInt(userInput.Quantity);
+        var userID = userInput.ID
+
+        for (var i = 0; i < res.length; i++) {     
+          console.log("DB Info: ", res[i].Stock_Quantity)    
+            itemCount = +res[i].Stock_Quantity;
+            itemsRemaining = itemCount - itemsOrdered;
+            
+            // total = parseInt(+(res[i].Price) * itemsOrdered);
+        }
+        console.log(itemCount)
+        console.log(userID)
+        console.log(itemsRemaining)
+        // get the information of the chosen item
+        // if(userInput.confirm) {
+        // userID = userInput.ID
+        // userQuantity = userInput.Quantity
+        
+        // console.log("\nYou've chosen Id number " + userID);
+        // console.log("Chosen quantity is " + userQuantity);  
+        // }else{
+        //   console.log("Transaction did not go through")
+        // }
+
+        connection.query(
+          
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              Stock_Quantity: itemsRemaining
+            },
+            {
+              Item_ID: userID
+            }
+          ],
+        )
+      });   
   });
 }
